193bcc6d191e98ba8f38c9e9cd94517c937183af
Elizabeth Dixon <cdixon855@gmail.com>
2022-07-14 13:14:13 -0500

    Create functions for quote gen api

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('Hello there')
+let apiQuotes = [];
+//Show New Quote
+function newQuote(){
+    //Pick a Random Quote from apiQuotes awway
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+// Get Quotes from API
+async function getQuotes(){
+const apiURL = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiURL);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        //Catch Error Here
+    }
+
+}
+
+//On Load
+getQuotes();
