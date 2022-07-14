ca6ceb982519236a4df2ee255eedbeace3e858a8
Elizabeth Dixon <cdixon855@gmail.com>
2022-07-14 17:54:49 -0500

    add js and remove the hardcoded text

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 let apiQuotes = [];
 //Show New Quote
 function newQuote(){
     //Pick a Random Quote from apiQuotes awway
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote);
+
+    //Check if Author field is blank and replace with 'Unknown'
+
+    if (!quote.author){
+        authorText.textContent = 'Unknown';
+    } else {
+      authorText.textContent = quote.author;
+    
+}
+//Check Quote Length to determine styling
+
+    if (quote.text.length > 120){
+        quoteText.classList.add('long-quote')
+    }else{
+        quoteText.classList.remove('long-quote')
+    }
+    quoteText.textContent = quote.text;
 }
 // Get Quotes from API
 async function getQuotes(){
